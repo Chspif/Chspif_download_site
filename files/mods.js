e39abb586d08b9603854157130b9ab2127f366ae
dp495 <hhxx201056@hotmail.com>
2026-05-27 00:02:51 +0800

    更新模组包

--- files/mods.js
+++ files/mods.js
@@ -6,12 +6,12 @@ const MODS_FILES = [
     },
     {
         name: '1.21.11懒人生电模组包',
-        uploadDate: '2026-05-17',
-        downloadUrl: 'https://chspif.oss-cn-hangzhou.aliyuncs.com/mods/1.21.11_fabric_%E4%B8%8D%E5%8D%A1%E9%94%AE%E6%A8%A1%E7%BB%84%E5%8C%85_dp495_260506.7z'
+        uploadDate: '2026-05-19',
+        downloadUrl: 'https://chspif.oss-cn-hangzhou.aliyuncs.com/mods/1.21.11_fabric_%E4%B8%8D%E5%8D%A1%E9%94%AE%E6%A8%A1%E7%BB%84%E5%8C%85_dp495.7z'
     },
     {
         name: '1.21.11完整模组包',
-        uploadDate: '2026-05-17',
-        downloadUrl: 'https://chspif.oss-cn-hangzhou.aliyuncs.com/mods/1.21.11_fabric_%E5%AE%A2%E6%88%B7%E7%AB%AF%E6%A8%A1%E7%BB%84%E5%8C%85_dp495_260506.7z'
+        uploadDate: '2026-05-19',
+        downloadUrl: 'https://chspif.oss-cn-hangzhou.aliyuncs.com/mods/1.21.11_fabric_%E5%AE%A2%E6%88%B7%E7%AB%AF%E6%A8%A1%E7%BB%84%E5%8C%85_dp495.7z'
     }
 ];

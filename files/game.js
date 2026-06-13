f66b2be1ca10b1fb866e44db4e3dfda1c652d74b
lyjdtz <1542761533@qq.com>
2026-06-13 15:41:51 +0800

    260613game modpacket

--- files/game.js
+++ files/game.js
@@ -3,5 +3,15 @@ const GAME_FILES = [
         name: '列车狼人杀V4.2公开版客户端导入包',
         uploadDate: '2026-05-17',
         downloadUrl: 'https://chspif.oss-cn-hangzhou.aliyuncs.com/game/StarRailHarpyExpress%20%E5%88%97%E8%BD%A6%E7%8B%BC%E4%BA%BA%E6%9D%80V4.2%E5%85%AC%E5%BC%80%E7%89%88%E5%AE%A2%E6%88%B7%E7%AB%AF%E5%AF%BC%E5%85%A5%E5%8C%85.zip'
+    },
+    {
+        name: '260613小游戏进入最低限度要求整合包',
+        uploadDate: '2026-06-13',
+        downloadUrl: 'https://chspif.oss-cn-hangzhou.aliyuncs.com/game/260613%E5%B0%8F%E6%B8%B8%E6%88%8F%E8%BF%9B%E5%85%A5%E6%89%80%E9%9C%80%E6%9C%80%E4%BD%8E%E6%95%B4%E5%90%88%E5%8C%85.zip'
+    },
+    {
+        name: '260613小游戏整合包-完整版',
+        uploadDate: '2026-06-13',
+        downloadUrl: 'https://chspif.oss-cn-hangzhou.aliyuncs.com/game/260613%E5%B0%8F%E6%B8%B8%E6%88%8F%E6%95%B4%E5%90%88%E5%8C%85-%E5%AE%8C%E6%95%B4%E7%89%88.zip'
     }
 ];

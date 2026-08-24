fe7ead0f3e5f76b16fe481c5529eaa0f54f0229f
dp495 <hhxx201056@hotmail.com>
2026-08-25 00:23:52 +0800

    更新模组包与资源包链接

--- files/mods.js
+++ files/mods.js
@@ -1,16 +1,21 @@
 const MODS_FILES = [
     {
-        name: '进入服务器最低要求mods',
-        uploadDate: '2026-05-17',
-        downloadUrl: 'https://chspif.oss-cn-hangzhou.aliyuncs.com/mods/mods.7z'
+        name: '26.2不卡键模组包',
+        uploadDate: '2026-08-25',
+        downloadUrl: 'https://www.chspif.website:667/s/b1205854c9764dda90'
     },
     {
-        name: '1.21.11懒人生电模组包',
+        name: '26.2全红石模组包',
+        uploadDate: '2026-08-25',
+        downloadUrl: 'https://www.chspif.website:667/s/b069e3d23870493cb4'
+    },
+    {
+        name: '1.21.11懒人生电模组包（非本周目）',
         uploadDate: '2026-05-19',
         downloadUrl: 'https://chspif.oss-cn-hangzhou.aliyuncs.com/mods/1.21.11_fabric_%E4%B8%8D%E5%8D%A1%E9%94%AE%E6%A8%A1%E7%BB%84%E5%8C%85_dp495.7z'
     },
     {
-        name: '1.21.11完整模组包',
+        name: '1.21.11完整模组包（非本周目）',
         uploadDate: '2026-05-19',
         downloadUrl: 'https://chspif.oss-cn-hangzhou.aliyuncs.com/mods/1.21.11_fabric_%E5%AE%A2%E6%88%B7%E7%AB%AF%E6%A8%A1%E7%BB%84%E5%8C%85_dp495.7z'
     }
